2eec73652a58c184e228a95a9444ac0fb99e182a
lowrybg <lowrybg@users.noreply.github.com>
2017-03-28 00:26:27 +0300

    Add files via upload

--- PrintNumbers/Numbers.cs
+++ PrintNumbers/Numbers.cs
@@ -1,6 +1,4 @@
-﻿/*Problem 6. Print Numbers
-
-    Write a program to print the numbers 1, 101 and 1001, each at a separate line.
+ /*Write a program to print the numbers 1, 101 and 1001, each at a separate line.
     Name the program correctly.
 */
 using System;
@@ -13,4 +11,3 @@ class Numbers
         Console.WriteLine("1001");
     }
 }
-
